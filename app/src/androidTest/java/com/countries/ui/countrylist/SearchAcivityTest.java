74f988f3a61455b99833be38d3839e892ef490cb
Tosin <tosin@irokotv.com>
2017-05-25 12:30:38 +0100

    created Espresso UI test for Search

--- app/src/androidTest/java/com/countries/ui/countrylist/SearchAcivityTest.java
+++ app/src/androidTest/java/com/countries/ui/countrylist/SearchAcivityTest.java
@@ -1,6 +1,9 @@
 package com.countries.ui.countrylist;
 
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingPolicies;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,15 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
-
+import com.countries.ElapsedTimeIdlingResource;
 import com.countries.R;
 
 import org.hamcrest.Description;
@@ -28,58 +23,78 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeUnit;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class SearchAcivityTest {
+
+    private IdlingResource idlingResource;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void searchAcivityTest() {
-        ViewInteraction appCompatImageButton = onView(
-allOf(withId(R.id.search_icon_button),
-withParent(allOf(withId(R.id.toolbar),
-withParent(withId(R.id.appbar)))),
-isDisplayed()));
-        appCompatImageButton.perform(click());
-        
-        ViewInteraction appCompatEditText = onView(
-allOf(withId(R.id.search_edit_text),
-withParent(withId(R.id.toolbar)),
-isDisplayed()));
-        appCompatEditText.perform(replaceText("Swe"), closeSoftKeyboard());
-        
-        ViewInteraction recyclerView = onView(
-allOf(withId(R.id.countries_recyclerview), isDisplayed()));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
-        
-        ViewInteraction textView = onView(
-allOf(withId(R.id.capital), withText("Stockholm"),
-childAtPosition(
-childAtPosition(
-IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-1),
-0),
-isDisplayed()));
-        textView.check(matches(withText("Stockholm")));
-        
-        ViewInteraction textView2 = onView(
-allOf(withId(R.id.language), withText("Swedish"),
-childAtPosition(
-childAtPosition(
-IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
-1),
-0),
-isDisplayed()));
-        textView2.check(matches(withText("Swedish")));
-        
-        }
 
-        private static Matcher<View> childAtPosition(
+        waitFor(5000); // wait for UI to be properly loaded
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.search_icon_button),
+                        withParent(allOf(withId(R.id.toolbar),
+                                withParent(withId(R.id.appbar)))),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.search_edit_text),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("Swe"), closeSoftKeyboard());
+
+        waitFor(5000); // wait for UI to be properly loaded
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.countries_recyclerview), isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        waitFor(5000); // wait for UI to be properly loaded
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.capital), withText("Stockholm"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Stockholm")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.language), withText("Swedish"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("Swedish")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
         return new TypeSafeMatcher<View>() {
@@ -93,8 +108,24 @@ isDisplayed()));
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup)parent).getChildAt(position));
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
     }
+
+
+    public void waitFor(long waitingTime) {
+        //SystemClock.sleep(waitingTime);  // This is replaced with IdlingResource.
+        // Make sure Espresso does not time out
+        IdlingPolicies.setMasterPolicyTimeout(waitingTime * 2, TimeUnit.MILLISECONDS);
+        IdlingPolicies.setIdlingResourceTimeout(waitingTime * 2, TimeUnit.MILLISECONDS);
+        // Now we wait
+        idlingResource = new ElapsedTimeIdlingResource(waitingTime);
+        Espresso.registerIdlingResources(idlingResource);
     }
+
+    public void cleanUp(){
+        // Clean up
+        Espresso.unregisterIdlingResources(idlingResource);
+    }
+}
